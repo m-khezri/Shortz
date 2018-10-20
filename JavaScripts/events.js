13e5d81ca4c74e54d5054fac941d351538a1efde
mohammad <khezri@outlook.com>
2018-10-20 13:03:13 -0500

    coded events

--- JavaScripts/events.js
+++ JavaScripts/events.js
@@ -1,1 +1,32 @@
-//  [Attaching all event listeners]
+// SEARCH 
+const search = () => {
+    $('#search_input').keypress(function() {
+    let searchText = $('#search_input').val();
+    alert('searchText');
+});
+};
+
+
+// FILETR CARDS 
+
+const filetrCards = () => {
+
+    $('morning_btn').click(e) => {
+       alert ('This is morning');     
+    })
+
+    $('afternoon_btn').click(e) => {
+        alert ('This is morning');     
+     })
+
+     $('evening_btn').click(e) => {
+        alert ('This is morning');     
+     })
+
+     $('afterdark_btn').click(e) => {
+        alert ('This is morning');     
+     })
+
+};
+
+export {search, filetrCards}
--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,9 +1,13 @@
 import { movie_description } from './data/movieData.js';
 import { movieLocations } from './data/locationsdata.js';
+import { search } from './events.js';
+import { filetrCards } from './events.js';
 
 const initializeApp = () => {
 	movie_description();
 	movieLocations();
+	search();
+	filetrCards();
 };
 
 initializeApp();

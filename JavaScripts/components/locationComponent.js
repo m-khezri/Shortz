87a0a548416b824506fbdbe107f51dc3e2cf6d26
mohammad <khezri@outlook.com>
2018-10-24 19:05:26 -0500

    Done filtering

--- JavaScripts/components/locationComponent.js
+++ JavaScripts/components/locationComponent.js
@@ -2,10 +2,10 @@ const writeMovieLocation = (arrayOfLocations) => {
 	let domString = '';
 	arrayOfLocations.forEach((locations) => {
 		domString += `
-    	<div class="card w-25 m-2 p-0" style="min-width: 300px;">
+    	<div class="movieLoc card w-25 m-2 p-0" style="min-width: 300px;">
 		  <h6 class="card-header bg-warning text-center">${locations.name}</h6>
           <img class="mw-100 mh-100" src="${locations.location_image}">
-          <p class="card-body text-center">${locations.location}</p>
+          <p class="card-address card-body text-center">${locations.location}</p>
 		  <p class="card-footer text-center font-weight-bold">${locations.shoot_time}</p>
 		</div>`;
 	});

a3db505f07a8cb5b05a6e08336e24d789799b5d4
mohammad <khezri@outlook.com>
2018-10-30 13:27:35 -0500

    movie locations

--- JavaScripts/components/movieComponent.js
+++ JavaScripts/components/movieComponent.js
@@ -2,15 +2,16 @@ const writeMovie = (arrayOfMovie) => {
 	let domString = '';
 	arrayOfMovie.forEach((movie) => {
 		domString += `
-		<div class="card style= "height: 20vh;">
-			<img class="card-img-top position-absolute mw-100" src="${movie.picture}">
-			<div class="card-img-overlay">			
-			<p class="card-title w-75 m-4 text-light display-2">${movie.Name}</h3>
-			<h5 class="card-text- w-50 m-4">Genre: ${movie.Genre}</h6>
-			<p class="card-text w-25 m-4">${movie.Description}</p>	
-			<h5 class="card-text w-50 m-4">${movie.Estimated_Release_Date}</h5>
-			</div>
-	    </div>`;
+        <div class="card w-25 m-1 p-0" style="min-width: 400px;">
+          <img class="card-img-top" style="width: 10rem" src="${movie.picture}">
+              <div class="card-body"> 
+              <h1 class="card-title m-1 text-danger">${movie.Name}</h1>
+              <h5 class="card-text m-1">Genre: ${movie.Genre}</h6>
+              <p class="card-text m-1">${movie.Description}</p>	
+              <h5 class="card-text my-2 text-primary">Number of locations: ${movie.locations.length}</h5>	
+              </div>
+          <h5 class="card-footer m-1">${movie.Estimated_Release_Date}</h5>
+          </div>`;
 	});
 	$('#movie_description').append(domString);
 };
